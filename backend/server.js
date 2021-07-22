da92d74ef61529546f9fb9178b605a2beaf2ac50
codexprt2 <codexprt2@gmail.com>
2021-07-22 12:44:21 +0530

    connect mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
 import data from "./data.js";
+import dotenv from "dotenv";
+import userRouter from "./routers/userRouter.js";
+
+const port = 5500;
+// const dbUrl = `process.env.MONGODB_URL`;
 
 const app = express();
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/amazona", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+connectDB();
 app.get("/api/products/:id", (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
@@ -15,11 +34,15 @@ app.get("/api/products/:id", (req, res) => {
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
-
+app.use("/api/users", userRouter);
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
-const port = process.env.PORT || 5000;
+
+app.use((err, req, res) => {
+  res.status(500).send({ message: err.message });
+});
+
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
