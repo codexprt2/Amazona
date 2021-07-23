a85388206127f59b9489e2ade5970fafcd6c9efe
codexprt2 <codexprt2@gmail.com>
2021-07-23 11:10:53 +0530

    implement placeorder action

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
+import orderRouter from "./routers/orderRouter.js";
 
 dotenv.config();
 // const port = 5000;
@@ -29,6 +30,7 @@ connectDB();
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -2,6 +2,7 @@ import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
   CART_SAVE_SHIPPING_ADDRESS,
+  CART_EMPTY,
   CART_SAVE_PAYMENT_METHOD,
 } from "../constants/cartConstants";
 
@@ -30,7 +31,8 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
 
     case CART_SAVE_PAYMENT_METHOD:
       return { ...state, paymentMethod: action.payload };
-
+    case CART_EMPTY:
+      return { ...state, cartItems: [] };
     default:
       return state;
   }
