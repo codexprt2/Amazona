58e0a5cfd2ed1347723290bf78f21247939dad8a
codexprt2 <codexprt2@gmail.com>
2021-07-24 11:33:41 +0530

    delete product

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,6 +12,7 @@ import {
   productListReducer,
   productCreateReducer,
   productUpdateReducer,
+  productDeleteReducer,
 } from "./reducers/productReducers";
 import {
   userDetailsReducer,
@@ -50,6 +51,7 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
+  productDelete: productDeleteReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
